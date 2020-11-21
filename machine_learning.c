7e6475aab32076dff71e2ed2332f1e846bd948de
Yerzhaisang <55043014+Yerzhaisang@users.noreply.github.com>
2020-11-21 11:29:19 +0300

    Update machine_learning.c

--- machine_learning.c
+++ machine_learning.c
@@ -21,219 +21,148 @@
 
 #include "aqo.h"
 
-static double fs_distance(double *a, double *b, int len);
-static double fs_similarity(double dist);
-static double compute_weights(double *distances, int nrows, double *w, int *idx);
-
-
-/*
- * Computes L2-distance between two given vectors.
- */
 double
-fs_distance(double *a, double *b, int len)
+neural_predict (int nfeatures, double **W1, double *b1, double **W2, double *b2, double *W3, double b3, double *feature) //prediction
 {
-	double		res = 0;
-	int			i;
-
-	for (i = 0; i < len; ++i)
-		res += (a[i] - b[i]) * (a[i] - b[i]);
-	if (len != 0)
-		res = sqrt(res / len);
-	return res;
+    double *out1;
+    double *out2;
+    double *out3;
+    double *out4;
+    double out5;
+    out1 = palloc0(WIDTH_1 * sizeof(*out1));
+    for (int i = 0; i < WIDTH_1; ++i){
+        for (int j = 0; j < nfeatures; ++j)
+            out1[i] = out1[i]+feature[j]*W1[i][j];
+        out1[i]=out1[i]+b1[i];
+    }
+    out2 = palloc0(WIDTH_1 * sizeof(*out2)); // vector for the output of Leaky ReLU activation function in the first layer
+    for (int i = 0; i < WIDTH_1; ++i){
+        if (out1[i]<out1[i]*slope)
+            out2[i]=out1[i]*slope;
+        else
+            out2[i]=out1[i];
+    }
+    out3 = palloc0(WIDTH_2 * sizeof(*out3));
+    for (int i = 0; i < WIDTH_2; ++i){
+        for (int j = 0; j < WIDTH_1; ++j)
+            out3[i]=out3[i]+out2[j]*W2[i][j];
+        out3[i]=out3[i]+b2[i];
+    }
+    out4 = palloc0(WIDTH_2 * sizeof(*out4)); // vector for the output of Leaky ReLU activation function in the second layer
+    for (int i = 0; i < WIDTH_2; ++i){
+        if (out3[i]<out3[i]*slope)
+            out4[i]=out3[i]*slope;
+        else
+            out4[i]=out3[i];
+    }
+    out5=0; //final result (one number)
+    for (int j = 0; j < WIDTH_2; ++j)
+        out5=out5+out4[j]*W3[j];
+    out5=out5+b3;
+    pfree(out1);
+    pfree(out2);
+    pfree(out3);
+    pfree(out4);
+    return out5;
 }
 
-/*
- * Returns similarity between objects based on distance between them.
- */
-double
-fs_similarity(double dist)
+void
+neural_learn (int nfeatures, double **W1, double *b1, double **W2, double *b2, double *W3, double b3, double *feature, double target)
 {
-	return 1.0 / (0.001 + dist);
-}
-
-/*
- * Compute weights necessary for both prediction and learning.
- * Creates and returns w, w_sum and idx based on given distances ad matrix_rows.
- *
- * Appeared as a separate function because of "don't repeat your code"
- * principle.
- */
-double
-compute_weights(double *distances, int nrows, double *w, int *idx)
-{
-	int		i,
-			j;
-	int		to_insert,
-			tmp;
-	double	w_sum = 0;
-
-	for (i = 0; i < aqo_k; ++i)
-		idx[i] = -1;
-
-	/* Choose from all neighbors only several nearest objects */
-	for (i = 0; i < nrows; ++i)
-		for (j = 0; j < aqo_k; ++j)
-			if (idx[j] == -1 || distances[i] < distances[idx[j]])
-			{
-				to_insert = i;
-				for (; j < aqo_k; ++j)
-				{
-					tmp = idx[j];
-					idx[j] = to_insert;
-					to_insert = tmp;
-				}
-				break;
-			}
-
-	/* Compute weights by the nearest neighbors distances */
-	for (j = 0; j < aqo_k && idx[j] != -1; ++j)
-	{
-		w[j] = fs_similarity(distances[idx[j]]);
-		w_sum += w[j];
-	}
-	return w_sum;
-}
-
-/*
- * With given matrix, targets and features makes prediction for current object.
- *
- * Returns negative value in the case of refusal to make a prediction, because
- * positive targets are assumed.
- */
-double
-OkNNr_predict(int nrows, int ncols, double **matrix, const double *targets,
-			  double *features)
-{
-	double	distances[aqo_K];
-	int		i;
-	int		idx[aqo_K]; /* indexes of nearest neighbors */
-	double	w[aqo_K];
-	double	w_sum;
-	double	result = 0;
-
-	for (i = 0; i < nrows; ++i)
-		distances[i] = fs_distance(matrix[i], features, ncols);
-
-	w_sum = compute_weights(distances, nrows, w, idx);
-
-	for (i = 0; i < aqo_k; ++i)
-		if (idx[i] != -1)
-			result += targets[idx[i]] * w[i] / w_sum;
-
-	if (result < 0)
-		result = 0;
-
-	/* this should never happen */
-	if (idx[0] == -1)
-		result = -1;
-
-	return result;
-}
-
-/*
- * Modifies given matrix and targets using features and target value of new
- * object.
- * Returns indexes of changed lines: if index of line is less than matrix_rows
- * updates this line in database, otherwise adds new line with given index.
- * It is supposed that indexes of new lines are consequent numbers
- * starting from matrix_rows.
- */
-int
-OkNNr_learn(int nrows, int nfeatures, double **matrix, double *targets,
-			double *features, double target)
-{
-	double	   distances[aqo_K];
-	int			i,
-				j;
-	int			mid = 0; /* index of row with minimum distance value */
-	int		   idx[aqo_K];
-
-	/*
-	 * For each neighbor compute distance and search for nearest object.
-	 */
-	for (i = 0; i < nrows; ++i)
-	{
-		distances[i] = fs_distance(matrix[i], features, nfeatures);
-		if (distances[i] < distances[mid])
-			mid = i;
-	}
-
-	/*
-	 * We do not want to add new very similar neighbor. And we can't
-	 * replace data for the neighbor to avoid some fluctuations.
-	 * We will change it's row with linear smoothing by learning_rate.
-	 */
-	if (nrows > 0 && distances[mid] < object_selection_threshold)
-	{
-		for (j = 0; j < nfeatures; ++j)
-			matrix[mid][j] += learning_rate * (features[j] - matrix[mid][j]);
-		targets[mid] += learning_rate * (target - targets[mid]);
-
-		return nrows;
-	}
-
-	if (nrows < aqo_K)
-	{
-		/* We can't reached limit of stored neighbors */
-
-		/*
-		 * Add new line into the matrix. We can do this because matrix_rows
-		 * is not the boundary of matrix. Matrix has aqo_K free lines
-		 */
-		for (j = 0; j < nfeatures; ++j)
-			matrix[nrows][j] = features[j];
-		targets[nrows] = target;
-
-		return nrows+1;
-	}
-	else
-	{
-		double	*feature;
-		double	avg_target = 0;
-		double	tc_coef; /* Target correction coefficient */
-		double	fc_coef; /* Feature correction coefficient */
-		double	w[aqo_K];
-		double	w_sum;
-
-		/*
-		 * We reaches limit of stored neighbors and can't simply add new line
-		 * at the matrix. Also, we can't simply delete one of the stored
-		 * neighbors.
-		 */
-
-		/*
-		 * Select nearest neighbors for the new object. store its indexes in
-		 * idx array. Compute weight for each nearest neighbor and total weight
-		 * of all nearest neighbor.
-		 */
-		w_sum = compute_weights(distances, nrows, w, idx);
-
-		/*
-		 * Compute average value for target by nearest neighbors. We need to
-		 * check idx[i] != -1 because we may have smaller value of nearest
-		 * neighbors than aqo_k.
-		 * Semantics of coef1: it is defined distance between new object and
-		 * this superposition value (with linear smoothing).
-		 * */
-		for (i = 0; i < aqo_k && idx[i] != -1; ++i)
-			avg_target += targets[idx[i]] * w[i] / w_sum;
-		tc_coef = learning_rate * (avg_target - target);
-
-		/* Modify targets and features of each nearest neighbor row. */
-		for (i = 0; i < aqo_k && idx[i] != -1; ++i)
-		{
-			fc_coef = tc_coef * (targets[idx[i]] - avg_target) * w[i] * w[i] /
-				sqrt(nfeatures) / w_sum;
-
-			targets[idx[i]] -= tc_coef * w[i] / w_sum;
-			for (j = 0; j < nfeatures; ++j)
-			{
-				feature = matrix[idx[i]];
-				feature[j] -= fc_coef * (features[j] - feature[j]) /
-					distances[idx[i]];
-			}
-		}
-	}
-
-	return nrows;
+    double *out1;
+    double *out2;
+    double *out3;
+    double *out4;
+    double out5, loss, dp1;
+    double *gradW3;
+    double *dp2;
+    double	*gradW2[WIDTH_2];
+    double *dp3;
+    double	*gradW1[WIDTH_1];
+    for (int k = 0; k < N_ITERS; ++k){
+        out1 = palloc0(WIDTH_1 * sizeof(*out1));
+        for (int i = 0; i < WIDTH_1; ++i){
+            for (int j = 0; j < nfeatures; ++j)
+                out1[i] = out1[i]+feature[j]*W1[i][j];
+            out1[i]=out1[i]+b1[i];
+        }
+        out2 = palloc0(WIDTH_1 * sizeof(*out2));
+        for (int i = 0; i < WIDTH_1; ++i){
+            if (out1[i]<out1[i]*slope)
+                out2[i]=out1[i]*slope;
+            else
+                out2[i]=out1[i];
+        }
+        out3 = palloc0(WIDTH_2 * sizeof(*out3));
+        for (int i = 0; i < WIDTH_2; ++i){
+            for (int j = 0; j < WIDTH_1; ++j)
+                out3[i]=out3[i]+out2[j]*W2[i][j];
+            out3[i]=out3[i]+b2[i];
+        }
+        out4 = palloc0(WIDTH_2 * sizeof(*out4));
+        for (int i = 0; i < WIDTH_2; ++i){
+            if (out3[i]<out3[i]*slope)
+                out4[i]=out3[i]*slope;
+            else
+                out4[i]=out3[i];
+        }
+        out5=0;
+        for (int j = 0; j < WIDTH_2; ++j)
+            out5=out5+out4[j]*W3[j];
+        out5=out5+b3; // output of forward pass
+        loss = pow(out5 - target,2); 
+        dp1 = (out5 - target) * 2; // derivative of the loss w.r.t. the output of forward pass
+        gradW3=palloc(WIDTH_2 * sizeof(*gradW3)); // vector for the gradient of the loss w.r.t. weight vector in the third layer
+        for (int i = 0; i < WIDTH_2; ++i)
+            gradW3[i] = dp1 * out4[i];
+        dp2=palloc(WIDTH_2 * sizeof(*dp2));
+        for (int i = 0; i < WIDTH_2; ++i)
+            dp2[i] = dp1 * W3[i];
+        for (int i = 0; i < WIDTH_2; ++i)
+            if (out3[i]<slope*out3[i])
+                dp2[i] = dp2[i]*slope;
+        gradW2[WIDTH_2]; // matrix for the gradient of the loss w.r.t. weight matrix in the second layer
+        for (int i = 0; i < WIDTH_2; ++i)
+            gradW2[i] = palloc(sizeof(**gradW2) * WIDTH_1);
+        for (int i = 0; i < WIDTH_2; ++i)
+            for (int j = 0; j < WIDTH_1; ++j)
+                gradW2[i][j]=dp2[i]*out2[j];
+        dp3=palloc0(sizeof(*dp3) * WIDTH_1);
+        for (int i = 0; i < WIDTH_1; ++i)
+            for (int j = 0; j < WIDTH_2; ++j)
+                dp3[j]+= dp2[j]*W2[j][i];
+        for (int i = 0; i < WIDTH_1; ++i)
+            if (out1[i]<slope*out1[i])
+                dp3[i] = dp3[i]*slope;
+        for (int i = 0; i < WIDTH_1; ++i)
+            gradW1[i] = palloc(sizeof(**gradW1) * nfeatures);
+        for (int i = 0; i < WIDTH_1; ++i)
+            for (int j = 0; j < nfeatures; ++j)
+                gradW1[i][j] = dp3[i] * feature[j];
+        for (int i = 0; i < WIDTH_1; ++i){
+            for (int j = 0; j < nfeatures; ++j)
+                W1[i][j] = W1[i][j] - lr*gradW1[i][j]; // updating the weights in the first layer
+            b1[i] = b1[i] - lr*dp3[i];
+        }
+        for (int i = 0; i < WIDTH_2; ++i){
+            for (int j = 0; j < WIDTH_1; ++j)
+                W2[i][j] = W2[i][j] - lr*gradW2[i][j]; // updating the weights in the second layer
+            b2[i] = b2[i] - lr*dp2[i];
+        }
+        for (int i = 0; i < WIDTH_2; ++i)
+            W3[i] = W3[i] - lr*gradW3[i]; // updating the weights in the third layer
+        b3 = b3 - lr*dp1;
+        if (nfeatures>0)
+        	for (int i = 0; i < WIDTH_1; ++i)
+        		pfree(gradW1[i]);
+        for (int i = 0; i < WIDTH_2; ++i)
+        	pfree(gradW2[i]);
+        pfree(gradW3);
+        pfree(out1);
+        pfree(out2);
+        pfree(out3);
+        pfree(out4);
+        pfree(dp2);
+        pfree(dp3);
+    }
 }
